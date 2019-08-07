0c348a3cdc9cd35c275fd8ebac56c86035647f14
adisarid <adisarid@gmail.com>
2019-08-07 16:12:55 +0300

    updated code for cv function

--- class code/03-cv_example.R
+++ class code/03-cv_example.R
@@ -39,19 +39,22 @@ cost_cv <- 10^seq(-3,4, by = 0.33)
 gamma_cv <- 10^seq(-4,3, by = 0.33)
 
 # Define the function to split the data, build the model, and return the miss classification error
-split_build_err <- function(k,...){
-  build_set <- movies_cls %>% 
-    sample_n(size = NROW(success)*(1-1/k))
-  validate_set <- movies_cls %>% 
+split_build_err <- function(data, k, class, ...){
+  
+  formula_svm <- as.formula(paste0(ensym(class), "~."))
+  
+  build_set <- data %>% 
+    sample_n(size = NROW(data)*(1-1/k))
+  validate_set <- data %>% 
     setdiff(build_set)
   
-  svm_model <- svm(formula = success ~ ., data = build_set, ...) # IMPORTANT: See the "pass the dots" use here.
+  svm_model <- svm(formula = formula_svm, data = build_set, ...) # IMPORTANT: See the "pass the dots" use here.
   
   svm_predict <- validate_set %>% 
     mutate(svm_pred = predict(svm_model, newdata = validate_set))
   
   svm_predict %>% 
-    mutate(errors = success != svm_pred) %>% 
+    mutate(errors = {{class}} != svm_pred) %>% 
     count(errors) %>% 
     mutate(err_rate = n/sum(n)) %>% 
     filter(errors) %>% 
@@ -60,9 +63,9 @@ split_build_err <- function(k,...){
 }
 
 # Example uses for split_build_err:
-split_build_err(k=10)
-split_build_err(k=10, cost = 5)
-split_build_err(k=10, kernel = "polynomial", degree = 3)
+split_build_err(movies_cls, k=10, class = success)
+split_build_err(movies_cls, k=10, class = success, cost = 5)
+split_build_err(movies_cls, k=10, class = success, kernel = "polynomial", degree = 3)
 
 # Running the cv with functional programming ----
 
@@ -72,14 +75,14 @@ pb <- progress_estimated(n = 10*length(cost_cv))
 error_results_cost <- crossing(iter = 1:10, cost = cost_cv) %>% 
   mutate(error_rates = map_dbl(iter, ~{
     pb$tick()$print()
-    split_build_err(k=10, cost = cost)
+    split_build_err(movies_cls, k=10, class = success, cost = cost)
     }))
 
 pb <- progress_estimated(n = 10*length(gamma_cv))
 error_results_gamma <- crossing(iter = 1:10, gamma = gamma_cv) %>% 
   mutate(error_rates = map_dbl(iter, ~{
     pb$tick()$print()
-    split_build_err(k=10, gamma = gamma)
+    split_build_err(movies_cls, k=10, class = success, gamma = gamma)
   }))
 
 error_results <- bind_rows(error_results_cost,
@@ -124,23 +127,12 @@ ggplot(error_results %>%
 
 
 # These two were not very beneficial, as there is no monotonicity that can be identified.
-degree_cv <- 1:15
-pb <- progress_estimated(n = 10*length(degree_cv))
-error_results_degree <- crossing(iter = 1:10, degree = degree_cv) %>% 
+
+
+set.seed(0)
+pb <- progress_estimated(n = 10*length(cost_cv))
+error_results_cost <- crossing(iter = 1:10, cost = cost_cv) %>% 
   mutate(error_rates = map_dbl(iter, ~{
     pb$tick()$print()
-    split_build_err(k=10, degree = degree, kernel = "polynomial")
+    split_build_err(iris, k=10, class = Species, cost = cost)
   }))
-
-error_results_degree <- read_csv("class code/03-svm-cv-degree-results.csv")
-
-ggplot(error_results_degree, 
-       aes(y = error_rates, group = degree)) + 
-  geom_boxplot()
-
-ggplot(error_results_degree %>% 
-         group_by(degree) %>% 
-         summarize(mean_err = mean(error_rates)), 
-       aes(y = mean_err, x = degree)) + 
-  geom_point() + 
-  geom_line()
