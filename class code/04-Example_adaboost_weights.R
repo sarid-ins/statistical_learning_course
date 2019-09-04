b1fb4a3d0b15014632f1e8423f84af47336649a0
adisarid <adi@sarid-ins.co.il>
2019-09-04 22:08:57 +0300

    code for creating the weights menti question

--- class code/04-Example_adaboost_weights.R
+++ class code/04-Example_adaboost_weights.R
@@ -44,12 +44,43 @@ for (m in 1:B) {
     rename_at(vars(curr_iteration), ~paste0("iteration", m+1))
 }
 
-# plot the weights changing:
+# plot the weights changing ----
 weights_for_plot <- track_weights %>% 
   mutate(obs_num = seq_along(iteration1)) %>% 
   gather(iteration, weight, -obs_num) %>% 
-  filter(obs_num %in% 1:10) %>% 
-  mutate(iteration = as.numeric(str_replace(iteration, "iteration", "")))
+  filter(obs_num %in% c(1:3, 9)) %>% 
+  mutate(iteration = as.numeric(str_replace(iteration, "iteration", ""))) %>% 
+  mutate(obs_num = ifelse(obs_num == 9, 5, obs_num))
 
 ggplot(weights_for_plot, aes(x = iteration, y = weight, color = factor(obs_num))) + 
-  geom_point() 
+  geom_point() + geom_line() +
+  facet_wrap(~ obs_num)
+
+# adding some decoys ----
+decoy1 <- tibble(obs_num = 0, 
+                  iteration = 1:51, 
+                  weight = c(sqrt(1:45)/1000, rep(0.001, 6)))
+decoy2 <- tibble(obs_num = 4,
+                 iteration = 1:51,
+                 weight = exp(-(1:51))/100)
+# plot with the decoys ----
+weights_all <- weights_for_plot %>%
+  bind_rows(decoy1,
+            decoy2) %>% 
+  mutate(name = recode_factor(obs_num,
+                              `0` = "Albatross",
+                              `1` = "Beaver",
+                              `2` = "Camel",
+                              `3` = "Deer",
+                              `4` = "Elephent",
+                              `5` = "Flamingo"))
+
+possible_weights_chart <- 
+  ggplot(weights_all, aes(x = iteration, y = weight, color = factor(name))) + 
+  geom_point() + geom_line() +
+  facet_wrap(~ name) + 
+  theme_bw()
+
+ggsave("c:/temp/possible_weights_chart.png", possible_weights_chart, width = 20, height = 10, units = "cm")
+
+# The albatross and the elephent are decoys.
