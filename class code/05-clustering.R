43965fba06bf5e548f4ef766d05e3f0e3fc8ee6f
adisarid <adi@sarid-ins.co.il>
2019-09-11 14:10:53 +0300

    added movies clustering and silhouette

--- class code/05-clustering.R
+++ class code/05-clustering.R
@@ -17,7 +17,36 @@ download_stats <- cranlogs::cran_downloads(packages = clustering_packages$packag
 
 # Cluster movies according to genre ---------------------------------------
 
+movies_raw <- read_csv("datasets/scraped_imdb/movie_db_clean.csv", col_types = cols())
 
+movies <- movies_raw %>% 
+  select(Action:Western, movie_title) %>% 
+  mutate_at(vars(-movie_title), as.numeric) %>% 
+  select(-News, -`Reality-TV`, -Short, -`Film-Noir`, `Game-Show`) # removing genres with few observations, I checked real quick with colSums
+
+# distrbution of number of categories per movie:
+appear_genres <- movies %>% 
+  gather(category, relevance, -movie_title) %>% 
+  filter(relevance == 1) %>% 
+  count(movie_title)
+
+ggplot(appear_genres, aes(n)) + 
+  stat_ecdf() + 
+  theme_bw() + 
+  ggtitle("Number of genres per movie (imdb scraped data)")
+
+
+# Cluster movies by genres using binary distance --------------------------
+# now to cluster the movies using cluster::pam (partitioning around medoids), binary distances
+
+movie_dist <- dist(select(movies, -movie_title), method = "binary")
+head(movie_dist, 10)
+
+movie_clust <- cluster::pam(x = movie_dist, k = 5)
+
+summary(movie_clust)
+
+factoextra::fviz_silhouette(movie_clust)
 
 # Cluster countries according to alchohol consumption ---------------------
 
