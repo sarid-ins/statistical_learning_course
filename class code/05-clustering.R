d4c21bf114abeaa629ec8bcfee0c43afce453979
adisarid <adi@sarid-ins.co.il>
2019-09-11 18:11:45 +0300

    added silhouette and elbow charts as rds files and respective code

--- class code/05-clustering.R
+++ class code/05-clustering.R
@@ -36,7 +36,7 @@ ggplot(appear_genres, aes(n)) +
   ggtitle("Number of genres per movie (imdb scraped data)")
 
 
-# Cluster movies by genres using binary distance --------------------------
+# Cluster movies by genres using binary distance k=5 ----------------------
 # now to cluster the movies using cluster::pam (partitioning around medoids), binary distances
 
 movie_dist <- dist(select(movies, -movie_title), method = "binary")
@@ -48,7 +48,71 @@ summary(movie_clust)
 
 factoextra::fviz_silhouette(movie_clust)
 
-# Cluster countries according to alchohol consumption ---------------------
+# view movies per cluster
+movie_clustered <- movies %>% 
+  mutate(cluster = movie_clust$clustering) 
+
+movie_clustered %>% 
+  select(-movie_title) %>% 
+  gather(genre, value, -cluster) %>% 
+  group_by(cluster, genre) %>% 
+  summarize(mean = mean(value)) %>% 
+  ungroup() %>% 
+  filter(mean >= 0.2) %>% 
+  ggplot(aes(y = mean, x = genre, fill = genre)) + 
+  geom_col() +
+  facet_grid(rows = ~cluster) + 
+  theme_bw() + 
+  coord_flip()
+
+
+# Investigate the optimal k using "elbow", silhouette, and the gap --------
+set.seed(0)
+# do not run this, takes too long to run!
+# examine_k <- NbClust::NbClust(data = select(movies, -movie_title) %>% 
+#                                 sample_n(1000) %>% 
+#                                 select_if(~{sum(.)>=150}),
+#                               distance = "binary",
+#                               min.nc = 1,
+#                               max.nc = 5,
+#                               index = "alllong",
+#                               method = "single")
+
+library(factoextra)
+
+# Elbow method
+# set.seed(0)
+# elbow_movies <- fviz_nbclust(x = select(movies, -movie_title),
+#                              cluster::pam, method = "wss",
+#                              diss = movie_dist,
+#                              k.max = 8, nboot = 100,
+#                              verbose = TRUE) +
+#   labs(subtitle = "Elbow method")
+# saveRDS(elbow_movies, file = "rds_files/elbow_movies.rds")
+elbow_movies <- readRDS("rds_files/elbow_movies.rds")
+
+# Silhouette method
+# silhouette_movies <- fviz_nbclust(x = select(movies, -movie_title),
+#                                   cluster::pam, method = "silhouette",
+#                                   diss = movie_dist,
+#                                   k.max = 8, nboot = 100,
+#                                   verbose = TRUE) +
+#   labs(subtitle = "Silhouette method")
+# saveRDS(silhouette_movies, "rds_files/silhouette_movies.rds")
+silhouette_movies <- readRDS("rds_files/silhouette_movies.rds")
+
+# Gap statistic
+set.seed(0)
+gap_stat_movies <- fviz_nbclust(x = select(movies, -movie_title),
+                                cluster::pam, method = "gap_stat",
+                                diss = movie_dist,
+                                k.max = 8, nboot = 100,
+                                verbose = TRUE) +
+  labs(subtitle = "Gap statistic method")
+saveRDS(gap_stat_movies, "rds_files/gap_stat_movies.rds")
+
+
+# Hierarchical clustering for movies --------------------------------------
 
 
 
